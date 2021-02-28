3af208b778921a2a8fc6cbab8edb8d077a3ef452
John Iliopoulos <svin@dismail.de>
2021-02-28 07:44:58 +0200

    can now download the raspberry pi binary

--- package/opt/classicube/run.sh
+++ package/opt/classicube/run.sh
@@ -2,12 +2,16 @@
 
 INSTDIR="${XDG_DATA_HOME-$HOME/.local/share}/classicube"
 
-if [ `getconf LONG_BIT` = "64" ]
-then
+if 	[ `uname -m` = "x86_64" ]; then
 	PACKAGE_LINK="nix64/ClassiCube.tar.gz"
-else
+elif [ `uname -m` = "i686" ]; then
 	PACKAGE_LINK="nix32/ClassiCube.tar.gz"
+else
+	PACKAGE_LINK="rpi32/ClassiCube.tar.gz"
+	#assume raspberry pi if all else is false
+	#not a good idea but it will work while i am searching for a solution
 fi
+
 
 deploy() {
 	mkdir -p $INSTDIR
